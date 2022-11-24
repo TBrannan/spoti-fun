9c47ed0db75062dca80c1a4071f50b17750ec813
Travis <glitche@gmail.com>
2022-11-23 20:55:24 -0500

    IDK anymore

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,111 +1,17 @@
-import { useState } from "react";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Client = (props) => {
-  const [token, setToken] = useState("");
-  const [searchKey, setSearchKey] = useState("");
-  const [tracks, setTracks] = useState([]);
-
-  const mobilesearchArtists = async (e) => {
-    e.preventDefault();
-    const local_token = await get_token();
-    const { data } = await axios
-      .get("https://api.spotify.com/v1/search", {
-        headers: {
-          Authorization: `Bearer ${local_token}`,
-        },
-        params: {
-          q: searchKey,
-          type: "track",
-        },
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    setTracks(data.tracks.items);
-  };
-
-  const get_token = async () => {
-    const { data } = await axios.get(process.env.REACT_APP_GET_ADDRESS, {});
-    setToken(data.token);
-    return data.token;
-  };
-
-  const Clicker = async (e, song_id, name) => {
-    e.preventDefault();
-    axios({
-      method: "post",
-      url: `https://api.spotify.com/v1/playlists/${process.env.REACT_APP_PLAYLIST_ID}/tracks?uris=spotify:track:${song_id}`,
-      data: {
-        grant_type: "client_credentials",
-      },
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    }).catch((err) => {
-      console.log(err);
-    });
-    toast(name + " Added");
-    update_playlist();
-  };
-
-  const update_playlist = async () => {
-    const { data } = await axios
-      .get(
-        `https://api.spotify.com/v1/playlists/${process.env.REACT_APP_PLAYLIST_ID}/tracks?`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .catch((err) => {
-        console.log(err);
-      });
-
-    const host_data = data.items;
-
-    localStorage.setItem("data", JSON.stringify(host_data.reverse()));
-  };
-
-  const renderArtists = () => {
-    return tracks.map((tracks) => (
-      <div
-        className="grid-container"
-        key={tracks.id}
-        value={tracks.id}
-        onClick={(e) => Clicker(e, tracks.id, tracks.name)}
-      >
-        <div className="limit">
-          {tracks.name}
-          <br></br>
-          {tracks.artists[0].name}
-        </div>
-        {tracks.album.images.length ? (
-          <img width={"100%"} src={tracks.album.images[0].url} alt="" />
-        ) : (
-          <div>No Image</div>
-        )}
-      </div>
-    ));
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Add Song to Spooky Playlist</h1>
-        <form onSubmit={mobilesearchArtists}>
-          <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
-          <button type={"submit"} onClick={mobilesearchArtists}>
-            Search
-          </button>
+        <form onSubmit={props.search}>
+          <input type="text" />
+          <button>Search</button>
         </form>
 
-        {renderArtists()}
+        {props.tracks ? props.render() : console.log("Empty")}
         <ToastContainer />
       </header>
     </div>
--- src/pages/Host.js
+++ src/pages/Host.js
@@ -1,56 +1,9 @@
-import { useEffect, useState } from "react";
-import "../App.css";
-
-const Host = () => {
-  const data_str = localStorage.getItem("data");
-  const final_data = JSON.parse(data_str);
-
-  const [list, setList] = useState(final_data);
-
-  useEffect(() => {
-    function checkUserData() {
-      const item = localStorage.getItem("data");
-      const parsed_data = JSON.parse(item);
-      console.log(parsed_data);
-
-      if (item) {
-        setList(parsed_data);
-      }
-    }
-
-    window.addEventListener("storage", checkUserData);
-
-    return () => {
-      localStorage.removeItem("storage", checkUserData);
-    };
-  }, []);
-
-  const renderArtists = () => {
-    return list.map((tracks) => (
-      <div
-        className="grid-container"
-        key={tracks.track.id}
-        value={tracks.track.id}
-      >
-        <div className="limit">
-          {tracks.track.name}
-          <br></br>
-          {tracks.track.artists[0].name}
-        </div>
-        {tracks.track.album.images.length ? (
-          <img width={"100%"} src={tracks.track.album.images[0].url} alt="" />
-        ) : (
-          <div>No Image</div>
-        )}
-      </div>
-    ));
-  };
-
+const Host = (props) => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spooky Playlist</h1>
-        {renderArtists()}
+        {console.log(props)}
       </header>
     </div>
   );
